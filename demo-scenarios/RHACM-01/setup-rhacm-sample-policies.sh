c6ef86e5d00a6823353ed79f8f18bb42f9a20fbb
Kasra <kasra.amirsarvari@gmail.com>
2025-02-24 21:02:06 +0000

    Update scripts

--- demo-scenarios/RHACM-01/setup-rhacm-sample-policies.sh
+++ demo-scenarios/RHACM-01/setup-rhacm-sample-policies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Define paths
-PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)"
+KUSTOMIZE_BASE="$PROJECT_DIR/demo-scenarios/RHACM-01/sample-rhacm-policies"
 
 # Load additional functions
 source "$PROJECT_DIR/automation/shell/lib/show_msg.sh"
@@ -9,6 +10,6 @@ source "$PROJECT_DIR/automation/shell/lib/run_cmd.sh"
 
 # Run Kustomize build and apply to OpenShift
 show_msg "show-date" "INFO" "Running Kustomize build..."
-kustomize build "$PROJECT_DIR/sample-rhacm-policies" | oc apply -f -
+kustomize build "$KUSTOMIZE_BASE" | oc apply -f -
 
 show_msg "show-date" "INFO" "Kustomize build and apply completed successfully."
