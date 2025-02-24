81e9b8abfe97318fc740f0305b73964db4a1e2b9
Kasra <kasra.amirsarvari@gmail.com>
2025-02-24 18:51:10 +0000

    Update example shell script

--- automation/shell/example-script/example.sh
+++ automation/shell/example-script/example.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
 # Get the directory of the script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)"
 
 # Load additional functions
-source "$SCRIPT_DIR/../lib/show_msg.sh"
-source "$SCRIPT_DIR/../lib/run_cmd.sh"
+source "$SCRIPT_DIR/lib/show_msg.sh"
+source "$SCRIPT_DIR/lib/run_cmd.sh"
 
 # Show message
 show_msg "show-date" "INFO" "Title of the example message" "Description of example message" "Body of the example message"
